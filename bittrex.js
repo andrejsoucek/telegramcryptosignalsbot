a1ec822418f31cc07b400b47cbd61ed9663d9ba7
Andrej Souček <andrej.soucek@antee.cz>
2017-12-26 00:21:50 +0100

    Initial commit

---
+++ bittrex.js
@@ -0,0 +1,198 @@
+class bittrex {
+    /**
+     * @param API_KEY
+     * @param SECRET
+     * @param btcAmount how many BTC are u willing to spend on each signal
+     * @param highestMarkup what is the highest markup to the signal you are wiling to set as a bid
+     * @param takeProfit take-profit steps in percentages { profit : amount }
+     * @param closeTimeLimit time in seconds how long to wait for the order to be closed before closing manually (MAX 900)
+     */
+    constructor(API_KEY, SECRET, btcAmount, highestMarkup, takeProfit, closeTimeLimit) {
+        this.bittrex  = require('node-bittrex-api');
+        this.bittrex.options({
+            'apikey' : API_KEY,
+            'apisecret' : SECRET
+        });
+        this.btcAmount = btcAmount;
+        this.highestMarkup = highestMarkup;
+        this.takeProfit = takeProfit;
+        this.maxTries = closeTimeLimit/10;
+        this.triesCounter = 0;
+    }
+
+    /**
+     * Check balance of BTC, if available and the price is within the markup bound, place the buy order
+     * @param currency
+     * @param price
+     */
+    checkBalancesAndBuy(currency, price) {
+        const that = this;
+        this.bittrex.getbalance({ currency : 'BTC' }, function( data, err ) {
+            if (err) {
+                console.log("Balance retrieval error: " + err.message);
+            }
+            if (data) {
+                if (data.result.Balance >= that.btcAmount) {
+                    const currencyPair = `BTC-${currency}`;
+                    that.bittrex.getorderbook({ market : currencyPair, type : "sell"}, function( data, err ) {
+                        if (err) {
+                            console.log("Order book error: " + err.message)
+                        }
+                        if (data) {
+                            const highestAsk = data.result[0].Rate;
+                            const maxPrice = price*that.highestMarkup;
+                            console.log("highest", highestAsk)
+                            console.log("maxAllowed", maxPrice)
+                            if (maxPrice > highestAsk) {
+                                that.buy(highestAsk, currencyPair, currency);
+                            } else {
+                                console.log("Asks are too high to buy for this price! Skipping this signal.")
+                            }
+                        }
+                    });
+                } else {
+                    console.log("insufficient BTC")
+                }
+            }
+        });
+    }
+
+    /**
+     * Placing the buy order
+     * @param buyPrice
+     * @param currencyPair
+     * @param currency
+     */
+    buy(buyPrice, currencyPair, currency) {
+        console.log(buyPrice)
+        const that = this;
+        const amount = this.btcAmount / buyPrice;
+        console.log(`===== PLACING LIMIT BUY ORDER (${currencyPair}) =====`);
+        this.bittrex.buylimit({market : currencyPair, quantity : amount, rate : btcPrice}, function ( data, err ) {
+            if (err) {
+                console.log("Order LIMIT BUY error: " + err.message);
+            }
+            if (data) {
+                console.log(new Date() + ` Placed order for ${amount} of ${currency} | Rate: ${$buyPrice} | Total BTC: ${amount*buyPrice} BTC |ID: ${data.result.uuid}`);
+                that.waitForClosing(data.result.uuid, buyPrice, currencyPair, currency);
+            }
+        });
+    }
+
+    /**
+     * Waiting until order is closed and sets the take-profits orders if so
+     * If it is not closed within given time, the order gets closed manually
+     * @param uuid
+     * @param buyPrice
+     * @param currencyPair
+     * @param currency
+     */
+    waitForClosing(uuid, buyPrice, currencyPair, currency) {
+        const that = this;
+        this.bittrex.getorder({ uuid : uuid }, function (data, err) {
+            if (err) {
+                console.log("Order status error: " + err.message);
+            }
+            if (data) {
+                if (data.result.IsOpen === true && that.triesCounter < that.maxTries) {
+                    setTimeout(function() {
+                        that.triesCounter++;
+                        that.waitForClosing(uuid, buyPrice, currencyPair, currency);
+                    }, 10000);
+                } else if (data.result.IsOpen === true && that.triesCounter >= that.maxTries) {
+                    that.closeOrder(data.result)
+                } else if (data.result.IsOpen === false) {
+                    that.checkBalanceAndSell(buyPrice, currencyPair, currency);
+                } else {
+                    throw new Error("Unexpected state of order. Terminating...");
+                }
+            }
+        })
+    }
+
+    /**
+     * @param order
+     */
+    closeOrder(order) {
+        console.log(`Cancelling order ${order.uuid}`)
+        this.bittrex.cancel({ uuid: order.uuid }, function( data, err ) {
+            if (err) {
+                console.log("Cancel order error: " + err.message);
+            }
+            if (data && data.success === true) {
+                console.log(`Cancelled order ID ${order.OrderUuid}: ${order.Type} ${order.Exchange}`)
+            }
+        })
+    }
+
+    /**
+     * Checks balance and places take-profit orders.
+     * @param buyPrice
+     * @param currencyPair
+     * @param currency
+     */
+    checkBalanceAndSell(buyPrice, currencyPair, currency) {
+        const that = this;
+        this.bittrex.getbalance({ currency : currency }, function( data, err ) {
+            if (err) {
+                console.log("Balance retrieval error: " + err.message);
+            }
+            if (data) {
+                const totalSellAmount = data.result.Balance;
+                console.log(`===== PLACING TAKE-PROFIT ORDERS (${currencyPair}) =====`);
+                const keys = Object.keys(that.takeProfit)
+                const last = keys[keys.length-1]
+                keys.forEach(function(k) {
+                    let amountMultiplier = parseFloat(that.takeProfit[k]) / 100;
+                    let priceMultiplier = 1 + parseFloat(k)/100;
+                    if (that.takeProfit.hasOwnProperty(k)) {
+                        if (k===last) {
+                            that.sellAll(currency, currencyPair, buyPrice * priceMultiplier);
+                        } else {
+                            that.sell(currency, currencyPair, totalSellAmount * amountMultiplier, buyPrice * priceMultiplier);
+                        }
+                    }
+                })
+            }
+        });
+    }
+
+    /**
+     * Sell given amount of given coins for given price
+     * @param currency
+     * @param currencyPair
+     * @param sellAmount
+     * @param sellPrice
+     */
+    sell(currency, currencyPair, sellAmount, sellPrice) {
+        this.bittrex.selllimit({market : currencyPair, quantity : sellAmount, rate : sellPrice}, function ( data, err ) {
+            if (err) {
+                console.log("Order LIMIT SELL error: " + err.message);
+            }
+            if (data) {
+                console.log(new Date() + ` Placed order to sell ${sellAmount} of ${currency} | Rate: ${sellPrice} | Total BTC: ${sellAmount*sellPrice} BTC | ID: ${data.result.uuid}`);
+            }
+        });
+    }
+
+    /**
+     * Sell all of the current balance
+     * @param currency
+     * @param currencyPair
+     * @param sellPrice
+     */
+    sellAll(currency, currencyPair, sellPrice) {
+        const that = this;
+        this.bittrex.getbalance({ currency : currency }, function( data, err ) {
+            if (err) {
+                console.log("Balance retrieval error: " + err.message);
+            }
+            if (data) {
+                const totalSellAmount = data.result.Balance;
+                that.sell(currency, currencyPair, totalSellAmount, sellPrice)
+            }
+        });
+    }
+}
+
+module.exports = bittrex;
---
+++ main.js
@@ -0,0 +1,106 @@
+const SimpleTelegram = require('simple-telegram')
+const Bittrex = require('./bittrex')
+const stg = new SimpleTelegram()
+
+/**
+ * Paths to bin and keys file to correctly launch telegram-cli
+ * @type {string}
+ */
+const tgBinFile  = "tg/bin/telegram-cli"
+const tgKeysFile = "tg/tg-server.pub"
+
+/**
+ * Bittrex settings
+ * @see bittrex.js docs
+ */
+const API_KEY = ''
+const SECRET = ''
+const btcAmount = 0.01
+const highestMarkup = 1.008
+const takeProfit = {10: 70, 20: 30}
+const closeTimeLimit = 90
+assertSettings();
+
+/**
+ * Settings to correctly recognize the signal and find the currency+price
+ * @type {RegExp}
+ */
+const signalGroupRegexp = /Signals group/
+const signalKeywordRegexp = /buy/
+const signalCurrencyRegexp = /^[\w]+/
+const signalPriceRegexp = /0?\.\d+/
+
+// Creating simpleTelegram object
+stg.create(tgBinFile, tgKeysFile)
+
+stg.getProcess().stdout.on("receivedMessage", function(msg) {
+    if (isSignal(msg)) {
+        console.log("Received signal! Processing...")
+        processSignal(msg.content)
+    }
+})
+
+/**
+ * Extracting currency (BTC-XXX) and price and placing an order
+ * @param s
+ */
+function processSignal(s) {
+    const currency = s.match(signalCurrencyRegexp)[0]
+    var price = s.match(signalPriceRegexp)[0]
+    if (price.charAt(0) === ".") {
+        price = 0 + price
+    }
+    if (currency && price) {
+        console.log('currency', currency)
+        console.log("price", price)
+        new Bittrex(API_KEY, SECRET, btcAmount, highestMarkup, takeProfit, closeTimeLimit)
+            .checkBalancesAndBuy(currency, parseFloat(price))
+    } else {
+        console.log("Could not find currency or price.")
+    }
+}
+
+/**
+ * Checks if the message is a signal or not
+ * @param msg
+ * @returns {boolean}
+ */
+function isSignal(msg) {
+    if (msg.caller.match(signalGroupRegexp) && msg.content.match(signalKeywordRegexp)) {
+        return true
+    } else {
+        return false
+    }
+}
+
+/**
+ * Checks if the settings are correct.
+ */
+function assertSettings() {
+    if (API_KEY.length <= 0 || SECRET <= 0) {
+        throw new Error("Please fill in the Bittrex API keys. Terminating...")
+    }
+    if (btcAmount > 0.5) {
+        console.log("WARNING: You are using a lot of money for altcoin trading. Supervising the bot is recommended.")
+    }
+    if (highestMarkup > 1.1) {
+        throw new Error("The markup is too high! Please set it to a lower value and try again. Terminating...")
+    }
+    var sum = 0
+    Object.keys(takeProfit).forEach(function(k) {
+        if (takeProfit.hasOwnProperty(k)) {
+            sum += takeProfit[k];
+        } else {
+            throw new Error("The take-profit object has some values missing. Terminating...")
+        }
+        if (k > 50) {
+            console.log("WARNING: Your take-profit steps are set to over 50%.")
+        }
+    })
+    if (sum!=100) {
+        throw new Error("The take-profit percentages must be set to give 100% together. Terminating...")
+    }
+    if (closeTimeLimit < 10 || closeTimeLimit > 900) {
+        throw new Error("The close time limit must be between 10 and 900 seconds.")
+    }
+}
